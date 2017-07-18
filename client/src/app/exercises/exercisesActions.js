3e2bdbc7b2586d1269a6d550f72cb1d2214dc549
cassaram09 <cassaram09@gmail.com>
2017-07-18 15:27:01 -0400

    add more CRUD functions to exerciseActions

--- client/src/app/exercises/exercisesActions.js
+++ client/src/app/exercises/exercisesActions.js
@@ -21,11 +21,37 @@ export function reducerAction(action, data) {
   return {type: types[action], data};
 }
 
+// for strong params in Rails
+const resourceName = 'exercise'
+
 const actions = {
+  queryExercises: queryExercises,
+  getExercise: getExercise
   createExercise: createExercise
+  updateExercise: updateExercise,
+  deleteExercise: deleteExericse
+}
+
+function queryExercises(){
+  return API.$query('/exercises')
+}
+
+function getExercise(exercise){
+  var resource = {name: 'exercise', data: exercise}
+  return API.$post('/exercises', resource)
 }
 
 function createExercise(exercise){
   var resource = {name: 'exercise', data: exercise}
   return API.$post('/exercises', resource)
+}
+
+function updateExercise(exercise){
+  var resource = {name: 'exercise', data: exercise}
+  return API.$patch('/exercises', resource)
+}
+
+function deleteExercise(exercise){
+  var resource = {name: 'exercise', data: exercise}
+  return API.$delete('/exercises', resource)
 }
