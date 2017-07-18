c7b3f4df727e8a72169e3fd92f8708a001279ccc
cassaram09 <cassaram09@gmail.com>
2017-07-18 13:24:59 -0400

    add resource routes under api/v1 scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  resources :routines
-  resources :workouts
-  resources :exercise_categories
-  resources :exercises
+  
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '/api/v1' do
+    resources :routines
+    resources :workouts
+    resources :exercise_categories
+    resources :exercises
+  end
+  
 end
