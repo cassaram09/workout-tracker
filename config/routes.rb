8a4730f6e6f66aa242b6ff222b56df4321a6c294
cassaram09 <cassaram09@gmail.com>
2017-07-18 12:58:36 -0400

    create exercise category resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :exercise_categories
   resources :exercises
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
