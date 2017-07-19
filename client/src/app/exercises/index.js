a1ae8e88e75a8311014d474f74d0d2c7c6403c10
cassaram09 <cassaram09@gmail.com>
2017-07-19 10:01:38 -0400

    add index module for exporting exercise components

---
+++ client/src/app/exercises/index.js
@@ -0,0 +1,5 @@
+export {default as ExercisesPage} from './exercisesPage'
+export {default as NewExercisePage} from './newExercisePage'
+export {default as ExercisePage} from './exercisePage'
+export {default as ExercisesList} from './exercisesList'
+export {default as ExerciseForm} from './exerciseForm'
--- client/src/app/routes.js
+++ client/src/app/routes.js
@@ -3,19 +3,17 @@ import { Route, IndexRoute} from 'react-router';
 
 import App from './App'
 import HomePage from './pages/homePage'
-import ExercisesPage from './exercises/exercisesPage'
-import NewExercisePage from './exercises/newExercisePage'
-import ExercisePage from './exercises/exercisePage'
 
+import * as exercises from './exercises/index'
 import * as workouts from './workouts/index'
 
 export default(
   // configure our routes - set App as our top level component with Home as the index route
   <Route path='/' component={App}>
     <IndexRoute component={HomePage}/>
-    <Route path='/exercises' component={ExercisesPage}>
-      <Route name='newExercise' path='/exercises/new' component={NewExercisePage} />
-      <Route name='exercise' path='/exercises/:id' component={ExercisePage} />
+    <Route path='/exercises' component={exercises.ExercisesPage}>
+      <Route name='newExercise' path='/exercises/new' component={exercises.NewExercisePage} />
+      <Route name='exercise' path='/exercises/:id' component={exercises.ExercisePage} />
     </Route>
     <Route path='/workouts' component={workouts.WorkoutsPage}>
       <Route name='newWorkout' path='/workouts/new' component={workouts.NewWorkoutPage} />
