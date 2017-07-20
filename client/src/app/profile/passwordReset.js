4944a468997ad4c24f468485b5c5a7e290986720
cassaram09 <cassaram09@gmail.com>
2017-07-20 18:21:50 -0400

    create password reset component

---
+++ client/src/app/profile/passwordReset.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux';  
+import {Grid, Row, Col } from 'react-bootstrap'
+
+import TextInput from '../common/textInput'
+import * as authActions from '../_auth/authActions'
+
+class PasswordReset extends Component {
+  constructor(props){
+    super(props);
+
+    this.state={
+      credentials: {
+        old_password: '',
+        password: '',
+        password_confirmation: '',
+      },
+      edit: false
+    }
+
+    // handle field changes
+    this.onChange = (event) =>{
+      const field = event.target.name;
+      const credentials = this.state.credentials;
+      credentials[field] = event.target.value
+      return this.setState({credentials: credentials})
+    }
+
+    // dispatches the API call action
+    this.onSave = (event) => {
+      event.preventDefault();
+      if (this.state.credentials.password == this.state.credentials.password_confirmation){
+        return this.props.auth.dispatchPasswordChange(this.state.credentials);
+      }
+    }
+
+    this.edit = () => {
+      return this.setState({edit: !this.state.edit})
+    }
+
+  }
+
+  render(){
+    if (this.state.edit){
+      return (
+        <div id="passwordReset">
+          <div className='field-group'>
+            <button className="btn btn-default" onClick={this.edit}>Close</button>
+          </div> 
+          <TextInput
+            name="old_password"
+            label="Old Password"
+            type="password"
+            value={this.state.credentials.old_password}
+            onChange={this.onChange}/>
+
+          <TextInput
+            name="password"
+            label="Password"
+            type="password"
+            value={this.state.credentials.password}
+            onChange={this.onChange}/>
+
+          <TextInput
+            name="password_confirmation"
+            label="Password confirmation"
+            type="password"
+            value={this.state.credentials.password_confirmation}
+            onChange={this.onChange}/>
+
+          <input
+            type="submit"
+            className="btn btn-primary"
+            onClick={this.onSave}/>
+        </div>
+      )
+    } else {
+      return (
+        <div id="passwordReset">
+          <div className='field-group'>
+            <button className="btn btn-default" onClick={this.edit}>Reset Password</button>
+          </div> 
+        </div>
+      )
+    }
+    
+  }
+
+}
+
+PasswordReset.propTypes = {
+
+}
+
+// map our sessionActions to class props
+function mapDispatchToProps(dispatch) {
+  return {
+    auth: bindActionCreators(authActions, dispatch)
+  }
+}
+
+export default connect(null, mapDispatchToProps)(PasswordReset);
--- client/src/app/profile/profile.js
+++ client/src/app/profile/profile.js
@@ -1,72 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux';  
 import {Grid, Row, Col } from 'react-bootstrap'
-
-import TextInput from '../common/textInput'
-import * as authActions from '../_auth/authActions'
+import PasswordReset from './passwordReset'
 
 class Profile extends Component {
-  constructor(props){
-    super(props);
-
-    this.state={
-      credentials: {
-        old_password: '',
-        password: '',
-        password_confirmation: '',
-      }
-    }
-
-    // handle field changes
-    this.onChange = (event) =>{
-      const field = event.target.name;
-      const credentials = this.state.credentials;
-      credentials[field] = event.target.value
-      return this.setState({credentials: credentials})
-    }
-
-    // dispatches the API call action
-    this.onSave = (event) => {
-      event.preventDefault();
-      if (this.state.credentials.password == this.state.credentials.password_confirmation){
-        this.props.auth.dispatchPasswordChange(this.state.credentials);
-      }
-    }
-
-  }
 
   render(){
     return (
       <Grid id="profile">
         <Row>
           <Col xs={8} sm={6} md={4} >
-            <TextInput
-              name="old_password"
-              label="Old Password"
-              type="password"
-              value={this.state.credentials.old_password}
-              onChange={this.onChange}/>
-
-            <TextInput
-              name="password"
-              label="Password"
-              type="password"
-              value={this.state.credentials.password}
-              onChange={this.onChange}/>
-
-            <TextInput
-              name="password_confirmation"
-              label="Password confirmation"
-              type="password"
-              value={this.state.credentials.password_confirmation}
-              onChange={this.onChange}/>
-
-            <input
-              type="submit"
-              className="btn btn-primary"
-              onClick={this.onSave}/>
+            <PasswordReset />
           </Col>
         </Row>
       </Grid>
@@ -79,11 +23,4 @@ Profile.propTypes = {
 
 }
 
-// map our sessionActions to class props
-function mapDispatchToProps(dispatch) {
-  return {
-    auth: bindActionCreators(authActions, dispatch)
-  }
-}
-
-export default connect(null, mapDispatchToProps)(Profile);
+export default Profile;
