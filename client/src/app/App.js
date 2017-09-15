dff0ceeb57e7884e0e6f1cfb5552f6a08c23be08
cassaram09 <cassaram09@gmail.com>
2017-09-14 21:53:21 -0400

    fix app component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';  
 
-import User from './users/userResource'
-import Store from './store/store'
-import Header from './common/header'
-import Auth from './auth/authComponent'
+import Header from './modules/common/header';
+import Auth from './modules/auth/authComponent';
 
 import './app.css'
 
 class App extends Component {
-  constructor(){
-    super()
-  }  
-
   render() {
     return (
-      <div className="App container">
+      <div className="app">
         <Auth />
         <Header  />
         {this.props.children}
