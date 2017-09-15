f354e936658ede303ae312e15b67e264876a3878
cassaram09 <cassaram09@gmail.com>
2017-09-14 22:46:17 -0400

    fix workoutcard

--- client/src/app/modules/workouts/workoutCard.js
+++ client/src/app/modules/workouts/workoutCard.js
@@ -1,19 +1,18 @@
 import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
-import moment from 'moment'
+import moment from 'moment';
 
-const workoutCard = ({workout}) => {
+function workoutCard({workout}) {
   var date = moment(workout.date).format('l')
   return(
     <Link to={`/workouts/${workout.id}`}>
       <div className='workout-card' style={{border: '1px dotted blue'}}>
-        <h4>{date} - {workout.name}</h4
-        >
+        <h4>{date} - {workout.name}</h4>
         <p>{workout.exercises.length} Exercises</p>
       </div>
     </Link>
   )
-};
+}
 
 workoutCard.propTypes = {
   workout: PropTypes.object.isRequired
