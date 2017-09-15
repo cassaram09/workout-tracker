9f0a3bfafc9d97c9df1490dddc216ea9fc1c3a27
cassaram09 <cassaram09@gmail.com>
2017-09-14 22:51:53 -0400

    fix workout Select

--- client/src/app/modules/workouts/workoutSelect.js
+++ client/src/app/modules/workouts/workoutSelect.js
@@ -1,46 +1,38 @@
 import React, {Component} from 'react';
-import { Link, IndexLink } from 'react-router';
-import moment from 'moment'
+import { Link, IndexLink, browserHistory } from 'react-router';
 import Select from 'react-select';
-import { browserHistory} from 'react-router';
 import 'react-select/dist/react-select.css';
+import moment from 'moment';
 
-class WorkoutSelect extends Component {
-  constructor(props){
-    super(props)
+function WorkoutSelect({workouts}) {
 
-    this.loadWorkout = (data) => {
-      if (data) {
-        browserHistory.push('/workouts/' + data.value)
-        this.value = data.label
-      }
+  const value;
+
+  const loadWorkout = (data) => {
+    if (data) {
+      browserHistory.push('/workouts/' + data.value)
+      value = data.label
     }
   }
-  
-  render(){
-    var options = [];
-    if (this.props.workouts.length > 0) {
-      options = this.props.workouts.map( (workout, index) => {
-        var date = moment(workout.date).format('L')
-        return {value: workout.id, label: `${date} - ${workout.name}`}
-      })
-    }
+
+  if (workouts.length > 0) {
+    options = this.props.workouts.map( (workout, index) => {
+      var date = moment(workout.date).format('L')
+      return {value: workout.id, label: `${date} - ${workout.name}`}
+    })
+  }
     
-
-    return (
-      <div>
-        <Select
-          name="form-field-name"
-          value={''}
-          options={options}
-          onChange={this.loadWorkout}
-        />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Select
+        name="form-field-name"
+        value={''}
+        options={[]}
+        onChange={loadWorkout}
+      />
+    </div>
+  )
   
 }
-
-
 
 export default WorkoutSelect;
