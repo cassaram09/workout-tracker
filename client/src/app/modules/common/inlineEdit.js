9c8df933713f2344160ac89348aad125a39de0ed
cassaram09 <cassaram09@gmail.com>
2017-09-14 22:57:28 -0400

    fix inlineEdit

--- client/src/app/modules/common/inlineEdit.js
+++ client/src/app/modules/common/inlineEdit.js
@@ -1,35 +1,33 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-class InlineEdit extends Component {  
-  constructor(){
-    super()
-
-    this.onChange = (event) => {
-      var string = event.target.value
-      this.props.onChange(string, this.props.name)
-    }
+function InlineEdit({name, value placeholder, onChange}) {  
+  
+  const onChangeHandler = (event) => {
+    onChange(event.target.value, name)
   }
 
-  render(){
-    return (
-      <div className="form-group">
-        <div className="field">
-          <input
-            type='text'
-            name={this.props.name}
-            className="form-control"
-            placeholder={this.props.placeholder}
-            value={this.props.value}
-            onChange={this.onChange}/>
-        </div>
+  return (
+    <div className="form-group">
+      <div className="field">
+        <input
+          type='text'
+          name={name}
+          className="form-control"
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}/>
       </div>
-    )
-
-  }
+    </div>
+  )
  
 }
 
-export default InlineEdit; 
+InlineEdit.propTypes = {  
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+};
 
- 
+export default InlineEdit;
