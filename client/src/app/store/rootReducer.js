4716e12c666b39f88d44b95658ed15a8ae901a70
cassaram09 <cassaram09@gmail.com>
2017-09-14 22:21:16 -0400

    fix root reducer

--- client/src/app/store/rootReducer.js
+++ client/src/app/store/rootReducer.js
@@ -1,9 +1,9 @@
 import {combineReducers} from 'redux';
 
-import Exercise from './modules/exercises/exerciseResource';
-import Workout from './modules/workouts/workoutResource';
-import User from './modules/user/userResource';
-import Auth from './modules/auth/authResource';
+import Exercise from '../modules/exercises/exerciseResource';
+import Workout from '../modules/workouts/workoutResource';
+import User from '../modules/user/userResource';
+import Auth from '../modules/auth/authResource';
 
 const rootReducer = combineReducers({
   exercises: Exercise.reducer, 
